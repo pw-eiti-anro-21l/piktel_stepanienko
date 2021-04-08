ebec7cdf2c35ab830795a3fb7f3a067f92608f56
Bartek <p.3bartek@gmail.com>
2021-04-08 12:18:57 +0200

    Added yaml configs

--- urdf_l2/setup.py
+++ urdf_l2/setup.py
@@ -21,7 +21,8 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*.py')),
-        (os.path.join('share', package_name), glob('urdf/*'))
+        (os.path.join('share', package_name), glob('urdf/*')),
+        (os.path.join('share', package_name), glob('config/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
